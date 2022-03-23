007f9a0800dbb7a6cb4a376ed3edbe8c514ed988
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-03-23 13:24:08 +0100

    SignUp and SignIn Auth with Firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
+import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -20,6 +23,33 @@ function SignUp() {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        
+            const user = userCredential.user
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+
+            const formDataCopy = {...formData}
+            delete formDataCopy.password
+            formDataCopy.timestamp = serverTimestamp()
+
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
             <div className='pageContainer'>
@@ -29,7 +59,7 @@ function SignUp() {
                     </p>
                 </header>
 
-                <form>
+                <form onSubmit={onSubmit}>
                     <input 
                         type="text"
                         className='nameInput'
